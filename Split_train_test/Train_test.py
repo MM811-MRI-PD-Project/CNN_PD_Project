8f8356c5c9ec5ae321f03ae78caca446fe9f81a9
IAMDXX <yiting5@ualberta.ca>
2018-11-11 12:15:14 -0700

    Add files via upload

---
+++ Split_train_test/Train_test.py
@@ -0,0 +1,6 @@
+from train_test_split import train_test_split_dir
+
+SOURCE_FILE_PATH = '/Users/DXX/Desktop/UACLASS/MM811/project/Datas/Slices'
+DEST_PATH = '/Users/DXX/Desktop/UACLASS/MM811/project/Datas/Test_Train' 
+
+train_test_split_dir(SOURCE_FILE_PATH,DEST_PATH)
